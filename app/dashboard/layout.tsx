3616e6a735cfa83db7de6d6dfde731ea93aa3036
lonzzi <lonzzi@qq.com>
2024-03-25 10:05:11 +0800

    feat: add dropdown menu

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,65 +1,15 @@
-import { cn } from '@/lib/utils';
-import type { SidebarNavItem } from '@/types';
-import { CircleEllipsis, Home, Search } from 'lucide-react';
-import Link from 'next/link';
+import { auth } from '@/auth';
 
 import { WithSidebar } from '@/components/with-sidebar';
 
-const SidebarItem: SidebarNavItem[] = [
-  {
-    title: 'home',
-    href: '#',
-    icon: Home,
-  },
-  {
-    title: 'google search',
-    href: 'https://google.com',
-    icon: Search,
-    external: true,
-  },
-];
+export default async function DashboardLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const session = await auth();
 
-export default function DashboardLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <WithSidebar sidebarContent={SidebarContent} mobileDashboardHeader={SidebarHeader}>
-      {children}
+    <WithSidebar user={session?.user}>
+      <div className="p-10">{children}</div>
     </WithSidebar>
   );
 }
-
-const SidebarHeader = () => {
-  return (
-    <div className="flex">
-      <span className="text-2xl font-bold">MS</span>
-    </div>
-  );
-};
-
-const SidebarContent = () => {
-  return (
-    <div className="h-full relative">
-      <SidebarHeader />
-      <div className="flex flex-col mt-6">
-        {SidebarItem.map((item, index) => (
-          <Link
-            href={item.href || '#'}
-            key={index}
-            className={cn(
-              'flex items-center p-2 text-sm font-medium text-gray-500 rounded-md hover:bg-gray-100',
-              item.disabled && 'opacity-50 cursor-not-allowed',
-              item.external && 'underline',
-            )}
-            target={item.external ? '_blank' : undefined}
-          >
-            <item.icon className="w-4 h-4" />
-            <span className="ml-2">{item.title}</span>
-          </Link>
-        ))}
-        <div className="flex items-center p-2 text-sm font-medium text-gray-500 rounded-md hover:bg-gray-100 absolute bottom-0 w-full cursor-pointer select-none">
-          <CircleEllipsis className="w-4 h-4" />
-          <span className="ml-2">more</span>
-        </div>
-      </div>
-    </div>
-  );
-};
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,25 +1,3 @@
-import { auth } from '@/auth';
-
-import { SignIn, SignOut } from '@/components/auth-components';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-
 export default async function Dashboard() {
-  const session = await auth();
-
-  return (
-    <div className="p-10">
-      <p>dashboard</p>
-      {session ? <SignOut /> : <SignIn />}
-      {session && (
-        <div className="flex items-center space-x-2">
-          <p>user: {session?.user?.name}</p>
-          <p>email: {session?.user?.email}</p>
-          <Avatar>
-            <AvatarImage src={session?.user?.image || ''} alt={session?.user?.name || ''} />
-            <AvatarFallback>{session?.user?.name}</AvatarFallback>
-          </Avatar>
-        </div>
-      )}
-    </div>
-  );
+  return <p>dashboard</p>;
 }
