43f905ae8cdda45e59069aa0bcde10fe5f581b58
lonzzi <lonzzi@qq.com>
2024-04-19 14:32:35 +0800

    fix: when using useSearchParams should wrapped with Suspense

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,12 @@
 import LoginForm from '@/app/ui/login-form';
+import { Suspense } from 'react';
 
 export default async function Page() {
   return (
     <div className="h-screen w-screen flex items-center justify-center">
-      <LoginForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
